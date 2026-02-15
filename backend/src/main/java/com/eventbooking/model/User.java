bbcaf32a031091b92f4fb43687d6ede9cf858c62
hackerstudent29 <hackerstudent2007@gmail.com>
2026-02-15 16:16:32 +0530

    Allow nulls for name column to fix schema migration

--- backend/src/main/java/com/eventbooking/model/User.java
+++ backend/src/main/java/com/eventbooking/model/User.java
@@ -18,7 +18,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
-    @Column(nullable = false)
+    @Column(nullable = true)
     private String name;
 
     @Column(nullable = false, unique = true)
